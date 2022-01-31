f99274ee3292b3d5b1dfbbd82e08ffda7654e9e6
GGillies-Productions <83219462+GGillies-Productions@users.noreply.github.com>
2022-01-31 21:34:23 +1000

    Add files via upload

--- massage website/javascript/index.js
+++ massage website/javascript/index.js
@@ -14,21 +14,23 @@ menuToggle.onclick = function(){
 }
 
 //random card function second page
-let card1 = document.querySelector('.card-1')
-let card2 = document.querySelector('.card-2')
-let card3 = document.querySelector('.card-3')
-let card4 = document.querySelector('.card-4')
-let card5 = document.querySelector('.card-5')
+
+let card3 = document.getElementById('card-3')
 let btn = document.querySelector('.btn')
 
-const cardPick = [card1, card2,  card4, card5 ]
+const cardPick = ["card-1", "card-2",  "card-4", "card-5" ]
 
 
-btn.onclick = function() {
+btn.addEventListener('click', () => {
     const random = function randomCard(cardPick){
         return cardPick [Math.floor(Math.random()*cardPick.length)];
     }
-    console.log(random(cardPick));
-}
+    
+    card3.id= (random(cardPick));
+})
 
-//console.log(random(cardPick));
+
+
+
+
+
